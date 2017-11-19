efe4a579341e3e1f7a19b73fcd0c7bb827c6e525
Ubuntu <ubuntu@ip-172-31-29-106.us-east-2.compute.internal>
2017-11-19 23:06:21 +0000

    Altug Gemalmaz Animation try

--- tutorial/js/line.js
+++ tutorial/js/line.js
@@ -1,21 +1,34 @@
+//var THREE = require('three');
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild( renderer.domElement );
 camera.position.z = 4;
 
 var material = new THREE.LineBasicMaterial({ color: 0x0000ff });
 var geometry = new THREE.Geometry();
 var j;
+var speed;
+var line;
+var asnc = 0;
 var x; var y; var z; var x1; var y1; var z1;
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 function path() 
-{	
+{//	requestAnimationFrame( animate );
+	// Compute Distance Between 2 Point
+		var dist = Math.sqrt(Math.pow((x1-x), 2)+Math.pow((y1-y), 2)+Math.pow((z1-z), 2));
+		console.log(dist);
+
+		
 		var geometry = new THREE.Geometry();
 		geometry.vertices.push(new THREE.Vector3(x,y,z), new THREE.Vector3(x1,y1,z1));
-		var line = new THREE.Line(geometry, material);
+		line = new THREE.Line(geometry, material);
 		scene.add(line);
 		renderer.render( scene, camera );
+
 }
 function assign()
 {
@@ -25,11 +38,12 @@ function assign()
 	x1 = parseFloat(str[j + 4]);
 	y1 = parseFloat(str[j + 5]);
 	z1 = parseFloat(str[j + 6]);
+	speed = parseFloat(str[j + 7]);
 }
 
-function draw()
+async function draw()
 {
-	var tmp = "L 0 0 0 0 0.89 0 10"
+	var tmp = "L 0 0 0 1 0 0 10 L 1 0 0 1 1 0 10";
 	str = tmp.split(" ");
 	var strLength = str.length;
 	var i = 0;
@@ -37,7 +51,16 @@ function draw()
 	{
 		j = i;
 		assign();
-		path();
+		var xs = (x1 - x)/500.0; var ys = (y1 - y ) / 500.0;
+        	var p = 0;
+        	for ( p = 0; p < 500; p++)
+		{
+                	x1 = x + p * xs;
+               		y1 = y + p * ys;
+                	path();
+        		await sleep(10);
+		}       
+
 	}
 }
 draw();
