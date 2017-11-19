757ffc6a0f66d7d3f5d5abbba82041ffb6cb7d6d
Ubuntu <ubuntu@ip-172-31-29-106.us-east-2.compute.internal>
2017-11-19 20:42:21 +0000

    Drawing lines according to the instructions from the GCODE

---
+++ tutorial/js/line.js
@@ -0,0 +1,43 @@
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+camera.position.z = 4;
+
+var material = new THREE.LineBasicMaterial({ color: 0x0000ff });
+var geometry = new THREE.Geometry();
+var j;
+var x; var y; var z; var x1; var y1; var z1;
+function path() 
+{	
+		var geometry = new THREE.Geometry();
+		geometry.vertices.push(new THREE.Vector3(x,y,z), new THREE.Vector3(x1,y1,z1));
+		var line = new THREE.Line(geometry, material);
+		scene.add(line);
+		renderer.render( scene, camera );
+}
+function assign()
+{
+	x = parseInt(str[j + 1]);
+	y = parseInt(str[j + 2]);
+	z = parseInt(str[j + 3]);
+	x1 = parseInt(str[j + 4]);
+	y1 = parseInt(str[j + 5]);
+	z1 = parseInt(str[j + 6]);
+}
+
+function draw()
+{
+	var tmp = "L 0 0 0 0 1 0 10"
+	str = tmp.split(" ");
+	var strLength = str.length;
+	var i = 0;
+	for (i = 0; i < strLength; i += 8)
+	{
+		j = i;
+		assign();
+		path();
+	}
+}
+draw();
---
+++ tutorial/server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+app.get('/',function(req,res)
+{
+	res.sendFile(path.join(__dirname + '/index.html'));
+});
+app.get('/js/three.js',function(req,res)
+{
+	res.sendFile(path.join(__dirname + '/js/three.js'));
+});
+
+app.get('/js/line.js',function(req,res)
+{
+	res.sendFile(path.join(__dirname + '/js/line.js'));
+});
+app.listen(8080);
